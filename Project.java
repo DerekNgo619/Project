4ea187cc8157ed0a97992416730a603de149eb2d
Derek Ngo <derek.ngo@mrjaffesclass.com>
2022-09-22 13:55:14 -0700

    1

---
+++ Project.java
@@ -0,0 +1,30 @@
+public class Project
+{
+    public static void main(String[] args) 
+    {
+        int a = 5;
+        int b = 2;
+        int d = -8;
+        if (a == 5) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
+        }
+        if (b == 2) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
+        }
+        if (b != 2) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
+        }
+        if (d == -8 && a == 4) {
+            System.out.println("SUCCESS");
+        } else {
+            System.out.println("FAILURE");
+        }
+        }
+    }
+
