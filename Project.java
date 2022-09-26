dc9976fd34076c8becd0cf26b1db00f605951051
Derek Ngo <derek.ngo@mrjaffesclass.com>
2022-09-26 14:00:19 -0700

    1

--- Project.java
+++ Project.java
@@ -6,19 +6,19 @@ public class Project
         int k = 0;
         int a = 0;
         int b = 0;
-        while (i < 15) {
+        do {
             System.out.println("i is " + i + " but is less than 15");
             i++;
-        }
-        while (k > 5) {
+            } while (i < 15);
+        do {
             System.out.println("k is " + k + " but is greater than 5");
             k++;
-        }
-        while (a < 10 && b <= 20) {
+        }    while (k > 5);
+        do {
             System.out.println("a is " + a + " but is less than 10");
             System.out.println("b is " + b + " but is less than or equal to 20");
             a++;
             b++;
+            } while (a < 10 && b <= 20);
         }
     }
-}
